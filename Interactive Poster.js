55112dbd7beb80527f91dc70ffbc1990b384f499
MiaMoreLuna <157151944+MiaMoreLuna@users.noreply.github.com>
2024-03-12 01:58:20 -0700

    Placement updates
    Updating placements

--- Interactive Poster.js
+++ Interactive Poster.js
@@ -1,10 +1,22 @@
 
+$ (document).ready (function() {
 
-	
-	
+$ (".answer") .hide ();
+$ ("#question") .click (function(){
+    $ ("#answer") .show ();
+})
+    }
 
-				
+$  ("#a1").hide ();
+$ ("#q1") .click (function() {
+    $ ("#a1") .show ();
+} );
 
+$ ("#a2") .hide ();
+$("#q2").click (function() {
+    $("#a2") .show ();
+    $("#a2") .toggle () ;
+});
 
 		
-	
+});
