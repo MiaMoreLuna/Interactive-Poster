89fb84b0606f9300a6f183b933fa45c609f6dbf3
MiaMoreLuna <157151944+MiaMoreLuna@users.noreply.github.com>
2024-03-15 13:18:04 -0700

    Updated Jquery and links

---
+++ js/script.js
@@ -0,0 +1,29 @@
+$ (document) .ready (function() {
+    
+$("#a2") .hide ();
+$("#q2").click (function(){
+    $("#a2").show ();
+});
+
+ $("#a3") .hide ();
+ $("#q3") .click (function(){
+    $("#a3").show ();
+ });
+
+ $("#a4") .hide ();
+ $("#q4").click (function(){
+    $("#a4").show ();
+ });
+
+ $("#Why").hide ();
+ $(".thewhy").click (function(){
+    $("#Why").show ();
+ });
+
+
+   
+ 
+});
+
+
+
